d310808699322e17dc0cd243c9284f674525f41b
Arganata-on <arganata.on@gmail.com>
2024-09-28 12:23:10 +0700

    Update Tugas Individu 02 - Harga Diskon.cpp

--- Tugas Individu 02 - Harga Diskon.cpp
+++ Tugas Individu 02 - Harga Diskon.cpp
@@ -3,12 +3,12 @@
 int main(){
     int hargaAsli = 150000;
     double diskon = 12.5;
-    int besarDiskon = hargaAsli * (diskon / 100);
-    int hargaSetelahDiskon = hargaAsli - besarDiskon;
+    int besarDiskon = hargaAsli * (diskon/100);
+    int hargaFinal = hargaAsli - besarDiskon;
 
     std::cout << "Harga : " << hargaAsli << " ribu" << '\n';
     std::cout << "Diskon : " << diskon << '\n'; 
     std::cout << "Besarnya diskon dari harga : " << hargaAsli * (diskon/100) << '\n';
-    std::cout << "Harga setelah diskon " << hargaSetelahDiskon << " ribu";
+    std::cout << "Harga setelah diskon " << hargaFinal << " ribu";
     return 0;
 }
